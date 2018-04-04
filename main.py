b76a510386cb988d3bc540c569c7e5b5eb3d89a6
mmolter <mmolter@mail.bradley.edu>
2018-04-04 09:54:02 -0500

    updated comments

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import urllib2
 
 def get_river_level():
-    ''' Return river level (ft) at PIAI2 river gauage. '''
+    ''' Return river level (ft) Hydrograph at PIAI2 river gauage. '''
 
     url = 'https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=piai2&output=tabular'
     response = urllib2.urlopen(url)
@@ -18,17 +18,19 @@ def get_river_level():
         raise AttributeError('Invalid response from NOAA')
 
 def mapit(x, in_min, in_max, out_min, out_max):
-    ''' Scale range of input x. '''
+    ''' Scale range of input x to match output range. '''
 
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def set_gauge(level, pin=0, freq=25, min_level=1, max_level=30, min_pwm=1, max_pwm=77):
-    ''' Set gauge dial to specified level (ft). '''
+    ''' Set physical gauge to specified level (ft). '''
     
     pwm = mapit(level, min_level, max_level, min_pwm, max_pwm)        
     os.system('fast-gpio pwm %s %s %s' % (pin, freq, pwm))
 
 if __name__ == '__main__':
+    ''' Retrieve river level and log. '''
+    
     logging.basicConfig(filename='/root/river_gauge/river_gauge.log', 
                         level=logging.DEBUG,
                         format='%(asctime)s %(message)s', 
